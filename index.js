383abacbba9dcd8b77cfd491ed2b2975a993ef5d
chenqianqian <595923592@qq.com>
2016-09-01 17:54:09 +0800

    add

---
+++ index.js
@@ -0,0 +1,15 @@
+init();
+
+  function init(){
+
+  	var slider =
+	  Swipe(document.getElementById('slider'), {
+	    auto: 2000,
+	    continuous: true
+	  });
+
+	  $(".close-icon").on("click", function(){
+	  	$("#slider").remove();
+	  })
+
+  }
